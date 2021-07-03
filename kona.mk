813ff3bf757f0e37e72e5a72452225cf6cf27349
Jiyong Park <jiyong@google.com>
2021-07-03 07:19:44 +0200

    sm8250-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of
    APEXes.
    
    Bug: 130418467
    Test: m
    Test: adb shell getprop ro.apex.updatable in Pixel 2 and 3
    Test: adb shell /system/apex/com.android.apex.cts.shim.apex exists.
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- kona.mk
+++ kona.mk
@@ -13,6 +13,9 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30 31 32
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
